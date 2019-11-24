0abe0ed3a5de595025997f31f408c01ab2ae786b
Shindi <vendingmachinesfullofsnacks@outlook.com>
2019-11-23 21:15:23 -0500

    Add files via upload

---
+++ zhang.js
@@ -0,0 +1,161 @@
+//-----------------------CAL---------------------------
+function myFunction1() {
+  var checkBox = document.getElementById("checkforcal");
+  var text = document.getElementById("CAL1");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction1a() {
+  var checkBox = document.getElementById("checkforcal");
+  var text = document.getElementById("CAL2");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction1b() {
+  var checkBox = document.getElementById("checkforcal");
+  var text = document.getElementById("CAL3");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+//-------------------CS115----------------------------
+function myFunction2() {
+  var checkBox = document.getElementById("checkforcs115");
+  var text = document.getElementById("CS115a");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction2a() {
+  var checkBox = document.getElementById("checkforcs115");
+  var text = document.getElementById("CS115b");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction2b() {
+  var checkBox = document.getElementById("checkforcs115");
+  var text = document.getElementById("CS115c");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction2c() {
+  var checkBox = document.getElementById("checkforcs115");
+  var text = document.getElementById("CS115d");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+//-----------------------CS146---------------------------
+function myFunction3() {
+  var checkBox = document.getElementById("checkforcs146");
+  var text = document.getElementById("CS146a");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction3a() {
+  var checkBox = document.getElementById("checkforcs146");
+  var text = document.getElementById("CS146b");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+//--------------------MA121------------------------------
+function myFunction4a() {
+  var checkBox = document.getElementById("checkforma");
+  var text = document.getElementById("MAa");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction4b() {
+  var checkBox = document.getElementById("checkforma");
+  var text = document.getElementById("MAb");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction4c() {
+  var checkBox = document.getElementById("checkforma");
+  var text = document.getElementById("MAc");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction4d() {
+  var checkBox = document.getElementById("checkforma");
+  var text = document.getElementById("MAd");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction4e() {
+  var checkBox = document.getElementById("checkforma");
+  var text = document.getElementById("MAe");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+//---------------PEP111---------------------------------
+function myFunction5a() {
+  var checkBox = document.getElementById("checkforpep");
+  var text = document.getElementById("PEPa");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+	
+  } else {
+     text.style.display = "none";
+  }
+}
+function myFunction5b() {
+  var checkBox = document.getElementById("checkforpep");
+  var text = document.getElementById("PEPb");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+//--------------------PEPF--------------------------------
+function myFunction6() {
+  var checkBox = document.getElementById("checkforpepf");
+  var text = document.getElementById("PEPF");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+
